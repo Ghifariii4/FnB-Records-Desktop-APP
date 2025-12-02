4c5d1da926ab1961c6e6a1f063ee5ffef09d8b81
Ahmad Ghifari <ahmadghifari831@gmail.com>
2025-12-02 10:27:22 +0700

    initial9

---
+++ FnB-Records/Login.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace FnB_Records
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            label8 = new Label();
+            label7 = new Label();
+            btnMasuk = new Guna.UI2.WinForms.Guna2Button();
+            label6 = new Label();
+            chkIngatSaya = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
+            label5 = new Label();
+            txtEmail = new Guna.UI2.WinForms.Guna2TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2GroupBox1
+            // 
+            guna2GroupBox1.BorderRadius = 10;
+            guna2GroupBox1.Controls.Add(label8);
+            guna2GroupBox1.Controls.Add(label7);
+            guna2GroupBox1.Controls.Add(btnMasuk);
+            guna2GroupBox1.Controls.Add(label6);
+            guna2GroupBox1.Controls.Add(chkIngatSaya);
+            guna2GroupBox1.Controls.Add(txtPassword);
+            guna2GroupBox1.Controls.Add(label5);
+            guna2GroupBox1.Controls.Add(txtEmail);
+            guna2GroupBox1.Controls.Add(label4);
+            guna2GroupBox1.Controls.Add(label3);
+            guna2GroupBox1.Controls.Add(label2);
+            guna2GroupBox1.Controls.Add(label1);
+            guna2GroupBox1.Controls.Add(guna2PictureBox1);
+            guna2GroupBox1.CustomBorderColor = Color.Transparent;
+            guna2GroupBox1.CustomizableEdges = customizableEdges11;
+            guna2GroupBox1.Font = new Font("Segoe UI", 9F);
+            guna2GroupBox1.ForeColor = Color.Transparent;
+            guna2GroupBox1.Location = new Point(295, 103);
+            guna2GroupBox1.Name = "guna2GroupBox1";
+            guna2GroupBox1.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            guna2GroupBox1.Size = new Size(544, 626);
+            guna2GroupBox1.TabIndex = 0;
+            guna2GroupBox1.Text = "guna2GroupBox1";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Cursor = Cursors.Hand;
+            label8.Font = new Font("Manrope SemiBold", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.FromArgb(45, 45, 45);
+            label8.Location = new Point(265, 530);
+            label8.Name = "label8";
+            label8.Size = new Size(180, 22);
+            label8.TabIndex = 12;
+            label8.Text = "Daftar gratis sekarang";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Manrope", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.FromArgb(125, 137, 149);
+            label7.Location = new Point(110, 530);
+            label7.Name = "label7";
+            label7.Size = new Size(159, 22);
+            label7.TabIndex = 11;
+            label7.Text = "Belum punya akun?";
+            // 
+            // btnMasuk
+            // 
+            btnMasuk.BorderRadius = 10;
+            btnMasuk.Cursor = Cursors.Hand;
+            btnMasuk.CustomizableEdges = customizableEdges1;
+            btnMasuk.DisabledState.BorderColor = Color.DarkGray;
+            btnMasuk.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnMasuk.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnMasuk.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnMasuk.FillColor = Color.FromArgb(45, 45, 45);
+            btnMasuk.Font = new Font("Manrope SemiBold", 14.2499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMasuk.ForeColor = Color.White;
+            btnMasuk.Location = new Point(44, 448);
+            btnMasuk.Name = "btnMasuk";
+            btnMasuk.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnMasuk.Size = new Size(455, 45);
+            btnMasuk.TabIndex = 4;
+            btnMasuk.Text = "Masuk";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Manrope", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(125, 137, 149);
+            label6.Location = new Point(70, 392);
+            label6.Name = "label6";
+            label6.Size = new Size(91, 22);
+            label6.TabIndex = 9;
+            label6.Text = "Ingat Saya";
+            // 
+            // chkIngatSaya
+            // 
+            chkIngatSaya.BackColor = Color.FromArgb(224, 224, 224);
+            chkIngatSaya.CheckedState.BorderColor = Color.FromArgb(211, 47, 47);
+            chkIngatSaya.CheckedState.BorderRadius = 2;
+            chkIngatSaya.CheckedState.BorderThickness = 0;
+            chkIngatSaya.CheckedState.FillColor = Color.FromArgb(211, 47, 47);
+            chkIngatSaya.CustomizableEdges = customizableEdges3;
+            chkIngatSaya.Location = new Point(44, 392);
+            chkIngatSaya.Name = "chkIngatSaya";
+            chkIngatSaya.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            chkIngatSaya.Size = new Size(20, 20);
+            chkIngatSaya.TabIndex = 3;
+            chkIngatSaya.Text = "guna2CustomCheckBox1";
+            chkIngatSaya.UncheckedState.BorderColor = Color.FromArgb(125, 137, 149);
+            chkIngatSaya.UncheckedState.BorderRadius = 2;
+            chkIngatSaya.UncheckedState.BorderThickness = 0;
+            chkIngatSaya.UncheckedState.FillColor = Color.FromArgb(125, 137, 149);
+            // 
+            // txtPassword
+            // 
+            txtPassword.BorderColor = Color.FromArgb(45, 45, 45);
+            txtPassword.BorderRadius = 10;
+            txtPassword.Cursor = Cursors.IBeam;
+            txtPassword.CustomizableEdges = customizableEdges5;
+            txtPassword.DefaultText = "";
+            txtPassword.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtPassword.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtPassword.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtPassword.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtPassword.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtPassword.Font = new Font("Inter", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtPassword.ForeColor = Color.FromArgb(45, 45, 45);
+            txtPassword.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtPassword.Location = new Point(44, 320);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderForeColor = Color.FromArgb(125, 137, 149);
+            txtPassword.PlaceholderText = "Masukan Password";
+            txtPassword.SelectedText = "";
+            txtPassword.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtPassword.Size = new Size(455, 45);
+            txtPassword.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Manrope", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(212, 122, 71);
+            label5.Location = new Point(44, 295);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 22);
+            label5.TabIndex = 6;
+            label5.Text = "Password";
+            // 
+            // txtEmail
+            // 
+            txtEmail.BorderColor = Color.FromArgb(45, 45, 45);
+            txtEmail.BorderRadius = 10;
+            txtEmail.Cursor = Cursors.IBeam;
+            txtEmail.CustomizableEdges = customizableEdges7;
+            txtEmail.DefaultText = "";
+            txtEmail.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtEmail.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtEmail.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtEmail.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtEmail.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtEmail.Font = new Font("Inter", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtEmail.ForeColor = Color.FromArgb(45, 45, 45);
+            txtEmail.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtEmail.Location = new Point(44, 230);
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderForeColor = Color.FromArgb(125, 137, 149);
+            txtEmail.PlaceholderText = "email@example.com";
+            txtEmail.SelectedText = "";
+            txtEmail.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtEmail.Size = new Size(455, 45);
+            txtEmail.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Manrope", 11.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(212, 122, 71);
+            label4.Location = new Point(44, 205);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 22);
+            label4.TabIndex = 4;
+            label4.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Inter", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(212, 122, 71);
+            label3.Location = new Point(122, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(323, 23);
+            label3.TabIndex = 3;
+            label3.Text = "Kelola Bisnis Kuliner Anda Dengan Mudah";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Manrope ExtraBold", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(212, 122, 71);
+            label2.Location = new Point(149, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(253, 33);
+            label2.TabIndex = 2;
+            label2.Text = "Masuk ke Akun Anda";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Manrope ExtraBold", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(212, 122, 71);
+            label1.Location = new Point(215, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(161, 33);
+            label1.TabIndex = 0;
+            label1.Text = "FnB Records";
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.BackColor = Color.Transparent;
+            guna2PictureBox1.BackgroundImage = Properties.Resources.Logo_FnB_Records_removebg_preview;
+            guna2PictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            guna2PictureBox1.CustomizableEdges = customizableEdges9;
+            guna2PictureBox1.FillColor = Color.Transparent;
+            guna2PictureBox1.Image = Properties.Resources.Logo_FnB_Records_removebg_preview;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(158, 15);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2PictureBox1.Size = new Size(71, 61);
+            guna2PictureBox1.TabIndex = 1;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(249, 247, 245);
+            ClientSize = new Size(1195, 827);
+            Controls.Add(guna2GroupBox1);
+            Font = new Font("Manrope", 8.999999F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.ControlText;
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            guna2GroupBox1.ResumeLayout(false);
+            guna2GroupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Guna.UI2.WinForms.Guna2TextBox txtEmail;
+        private Label label4;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox chkIngatSaya;
+        private Guna.UI2.WinForms.Guna2Button btnMasuk;
+        private Label label6;
+        private Label label8;
+        private Label label7;
+        private Guna.UI2.WinForms.Guna2TextBox txtPassword;
+    }
+}
--- FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
+++ FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05d6a8eedb7d77d578dee032920d731fe74494c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7f0440acc220ae39337f1981f9c5c4326d698e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
