3d6ae68ea40149aa4bcaaa80384ca255b015fd9c
Junioraja <jr.official331@gmail.com>
2025-12-02 18:19:50 +0700

    Add-UC

--- FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
+++ FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aeafa85a364e7f15af28215dddda303a25d1faa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c1c741f579154f27788f61f9a56327f5470baa")]
 [assembly: System.Reflection.AssemblyProductAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
