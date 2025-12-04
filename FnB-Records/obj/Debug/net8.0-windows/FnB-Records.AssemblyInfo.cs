2400dee4c0cf6ffbc0ce069389fd2441804fd187
Junioraja <jr.official331@gmail.com>
2025-12-04 15:02:33 +0700

    Program-Login

--- FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
+++ FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12164db4d1becdfd02937c17cf2234aa4bd1bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62c26400cc06f51b8289fe5d7101b13a4102897")]
 [assembly: System.Reflection.AssemblyProductAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
