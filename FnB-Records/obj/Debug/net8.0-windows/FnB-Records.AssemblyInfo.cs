ca3713a6f7abce4a2e95ec2b6185b8cd7eea2603
Ahmad Ghifari <ahmadghifari831@gmail.com>
2025-12-07 20:03:56 +0700

    Initial-13

--- FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
+++ FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fb80efc70d174c76e434e2381d98302a81665b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c92eb803b13747e0d2a53b9cb34593c75036a6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
