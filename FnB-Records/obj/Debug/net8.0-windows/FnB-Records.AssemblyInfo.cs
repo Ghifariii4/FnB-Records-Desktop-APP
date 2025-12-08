cd73f41dc13ac2252c22e835a16cbc389f6a420c
FaturDih <fathurtamvan69@gmail.com>
2025-12-08 08:11:15 +0700

    Tes

--- FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
+++ FnB-Records/obj/Debug/net8.0-windows/FnB-Records.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33ab836c361e8bdf588ff7a62a28c3d1f7e8cbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57789341df2ef08d1d5b6a61e595d59a4a025b1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FnB-Records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
